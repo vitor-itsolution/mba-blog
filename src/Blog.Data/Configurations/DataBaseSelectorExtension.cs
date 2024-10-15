d2f5630cd0ce6b0bc35693334e8975fb50c07fff
Vitor Coelho Rafael <vitor.itsolution@gmail.com>
2024-10-14 22:29:22 -0300

    Adjusts configuration

--- src/Blog.Data/Configurations/DataBaseSelectorExtension.cs
+++ src/Blog.Data/Configurations/DataBaseSelectorExtension.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Blog.Data.Context;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,12 @@ namespace Blog.Data.Configurations
                 builder.Services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             }
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
         }
     }
--- src/Blog.Web/Program.cs
+++ src/Blog.Web/Program.cs
@@ -1,19 +1,8 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Blog.Data.Context;
 using Blog.Data.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddDatabaseSelector();
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddControllersWithViews();
 
